14a3a1b3d6f2fcecf3453614331ebd758e12c580
sundayScoop <87186823+sundayScoop@users.noreply.github.com>
2022-06-19 17:23:08 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+from flask import *
+from ecdsa import SigningKey, NIST256p
+import requests
+app = Flask(__name__)
+
+key_list = {}
+id_count = 0
+
+@app.route('/key', methods=['GET'])
+def get_all_keys():
+    return jsonify(key_list)
+
+@app.route('/key/<id>', methods=['GET'])
+def get_key(id):
+
+    response = {
+        "key": key_list.get(int(id))
+    }
+
+    return jsonify(response)
+
+@app.route('/key', methods=['POST'])
+def add_key():
+    global id_count
+
+    new_key_string = request.form.get('key')
+    #new_key_string = addPemKeyStructure(new_key_string)
+
+    #new_secret_key = SigningKey.from_pem(new_key_string)
+
+    key_list[id_count] = new_key_string
+
+    response = {}
+    response['id'] = id_count
+    response['key'] = new_key_string
+
+    id_count += 1
+
+    return jsonify(response)
+
+def addPemKeyStructure(key):
+    top = "-----BEGIN EC PRIVATE KEY-----\n"
+    bottom = "\n-----END EC PRIVATE KEY-----"
+    key = top + key[:64] + "\n" + key[64:128] + "\n" + key[128:] + bottom
+    return key
+
+if __name__ == '__main__':
+    app.run()
