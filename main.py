3227d7d7d989db3d0866fb9297664b057b77427e
sundayScoop <87186823+sundayScoop@users.noreply.github.com>
2022-06-22 11:43:02 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,8 @@ from flask import *
 from ecdsa import SigningKey, NIST256p
 import base64
 import hashlib
-import requests
 from ecdsa.util import sigencode_der, sigdecode_der
-import hmac
-import json
-import http
+
 app = Flask(__name__)
 
 key_list = {}
@@ -17,54 +14,47 @@ def get_all_keys():
     global key_list
     return jsonify(key_list)
 
-@app.route('/Key/<id>', methods=['GET'])
-def get_key(id):
+@app.route('/Key/<ID>', methods=['GET'])
+def get_key(ID):
     global key_list
-    if int(id) in key_list:
+    if int(ID) in key_list:
+        
+        key = key_list.get(int(ID))
+
         response = {
-            "Key": key_list.get(int(id))
+            "Id": int(ID),    # Had to return ID even tho ID is supplied in URL
+            "Key": key
         }
-        #print("GETTTT" + str(id))
-        #print(key_list.get(int(id)))
-        print("GET")
-        print(str(response))
+       
         return jsonify(response)
     else:
-        return '', 500
+        return '', 500 # Had to specifically return status code 500
     
 @app.route('/Key', methods=['POST', 'PUT'])
 def add_key():
     global key_list
     global id_count
-    req = request.get_json()
     
+    req = request.get_json()
     key_string = req['Key']
+    
     if request.method == 'POST':
         key_id = id_count
-        #print("POST" + str(key_id))
-        #print(key_string)
-        #print("----")
+        
     elif request.method == 'PUT':
-        key_id = req['Id']
-        #print("PUT" + str(key_id))
-        #print(key_string)
-        #print("----")
-        
+        key_id = req['Id']  # Id is supplied in PUT request, but not POST
+       
+    key_list[key_id] = key_string  # Add key + key_id to dict
     
-    response = {}
-    
-    key_list[key_id] = key_string
+    if request.method == 'POST': # Could be simplified further, reducing IF statements
+        response = {} # Create reponse 'json'
+        response['Id'] = int(key_id)
+        response['Key'] = key_string
+        id_count+=1  # Because you added a key
+        return jsonify(response)
         
-    response['Id'] = int(key_id)
-    response['Key'] = key_string
-    
-    if request.method == 'POST': 
-        id_count+=1
-        
-    print(str(response))
-        
-    if request.method == 'POST': return jsonify(response)
-    else: return '', 200
+    elif request.method == 'PUT': 
+        return '', 200 # Return 200 OK
 
 @app.route('/Key/<id>', methods=['DELETE'])
 def del_key(id):
@@ -79,26 +69,19 @@ def encrypt_msg():
     global key_list
     
     key_id = request.args.get('keyId')
-    msg = base64.urlsafe_b64decode(request.args.get('message'))
+    msg = base64.urlsafe_b64decode(request.args.get('message')) # Decode from URL safe base64
 
     key_pem = key_list.get(int(key_id))
-    if key_pem.find("PRIVATE KEY") != -1:
-        secret_key = SigningKey.from_pem(key_pem, hashlib.sha3_256)    
-        signature = secret_key.sign_deterministic(msg, sigencode=sigencode_der)
-
     
-        encoded_sig = base64.b64encode(signature)
+    if key_pem.find("PRIVATE KEY") != -1: # Check key exists
+        secret_key = SigningKey.from_pem(key_pem, hashlib.sha3_256) # Create key from pem + sha3_256 hash
+        signature = secret_key.sign_deterministic(msg, sigencode=sigencode_der) # Sign key with openssl compatibility
+
+        encoded_sig = base64.b64encode(signature) # Encode in base64
 
         return (encoded_sig)
     else:
         return ''
-
-
-def addPemKeyStructure(key):
-    top = "-----BEGIN EC PRIVATE KEY-----\n"
-    bottom = "\n-----END EC PRIVATE KEY-----"
-    key = top + key[:64] + "\n" + key[64:128] + "\n" + key[128:] + bottom
-    return key
 
 if __name__ == '__main__':
     app.run(port=8080)
